473c240fabd1f9dda5e7f360030b0ccde2c139f0
aeguzsando <aeguzsando@gmail.com>
2016-08-28 00:32:26 -0500

    3rd Commit
    AG onemax using roulette selection operator, completed

--- java/AGoneMaxV03.java
+++ java/AGoneMaxV03.java
@@ -13,9 +13,9 @@ import java.util.Random;
 
 public class AGoneMaxV03 {
     
-    static int tam_genes = 3;   							
-    static int tam_poblacion =4; 
-    static int generaciones = 10; 
+    static int tam_genes = 100;   							
+    static int tam_poblacion =500; 
+    static int generaciones = 100000; 
     static List<List<Integer> > poblacion = new ArrayList<>();
     
     public static void main(String[] args){
@@ -23,29 +23,106 @@ public class AGoneMaxV03 {
         iniciarPoblacion();
         
         for (int i = 0; i < generaciones; i++) {
-            int i_padre1 = 0;
-            int i_padre2 = 0;
+            int indice_padre1;
+            int indice_padre2;
             int score_total = 0;
             
             for (int j = 0; j < tam_poblacion; j++) {
                 score_total += obtenerScore(poblacion.get(j));
             }
             
-             double[] prob =new double[tam_poblacion];
+             double[] prob = new double[tam_poblacion];
              for (int j = 0; j < tam_poblacion; j++) {
                prob[j]=obtenerScore(poblacion.get(j))/(double)score_total;
             }
             
-             //crearacumalad
-             int a=0;
-             a++;
-             System.out.println("Salida");
+             
+             //crear acumalada
+             
+             double[] acumulada = new double[poblacion.size()];
+             acumulada[0] = prob[0];
+             
+             for (int j = 1; j <poblacion.size(); j++) {
+                acumulada[j] = acumulada[j-1] + prob[j];
+            }
+            
+             acumulada[poblacion.size() - 1] = 1.0;
+             
+             // Determinar indices
+                // Indice padre 1
+             Random rnd = new Random();
+             double aleatorio;
+             // Generamos un numero aleatorio entre 0.0 y 1.0
+             aleatorio = rnd.nextDouble();
+             int index = 0;
+             
+             while (aleatorio > acumulada[index]) {                
+                index++;
+            }
+             
+             indice_padre1 = index;
+             
+                // Indice padre 2
+             aleatorio = rnd.nextDouble(); 
+             index = 0;
+             
+             while(aleatorio > acumulada[index]){
+                 index++;
+             }
+             
+             indice_padre2 = index;
+             
+             
+             // Creamos al hijo
+             
+             List<Integer> hijo =  new ArrayList<>();
+             
+             //Aplicamos el cruzamiento
+             
+             cruzamiento(indice_padre1, indice_padre2, hijo);
+             
+             // Mutacion de hijo
+             
+             mutacion(hijo);
+             
+             // Obtenemos al mejor de cada generacion
+             
+             int score_padre = obtenerScore(poblacion.get(indice_padre1));
+             int score_hijo = obtenerScore(hijo);
+             
+             //Copiamos en indice padre 1 en hijo si el score de este es mayor al del padre 
+             
+             if(score_hijo > score_padre){
+                 
+                 for (int j = 0; j < tam_genes; j++) {
+                      poblacion.get(indice_padre1).set(j, hijo.get(j));
+                 }
+             }
+             
+             System.out.println("Generacion"+(i+1));
+             System.out.println("Mejor");
+             
+            int indice_mejor = obtener_mejor();
+            int score_mejor =obtenerScore(poblacion.get(indice_mejor));
+            List<Integer> mejor = poblacion.get(indice_mejor);
+            
+            for (int j = 0; j < tam_genes; j++) 
+                System.out.print(" "+mejor.get(j)+" ");
+            
+            
+            System.out.println(" ");    
+            System.out.println("Puntuacion: "+score_mejor);
+            
+            if(score_mejor == tam_genes)
+                
+                break;    
+             
+//             int a=0;
+//             a++;
+//             System.out.println("Salida");
             
         }
-        
-        
-        
-        
+   
         
     }
 
@@ -72,6 +149,67 @@ public class AGoneMaxV03 {
             suma += individuo.get(i);
         }
       return suma;  
+    }
+
+    private static void cruzamiento(int indice_padre1, int indice_padre2, List<Integer> hijo) {
+        
+        Random rnd = new Random();
+        int punto1 = rnd.nextInt(tam_genes);
+	int punto2 = rnd.nextInt(tam_genes);
+                
+        if(punto1 < punto2){
+            for (int i = 0; i < punto1; i++){ 
+                Integer temp =poblacion.get(indice_padre1).get(i);
+                hijo.add(new Integer(temp.intValue()));
+            }
+            for (int i = punto1; i < punto2; i++) {
+                Integer temp =poblacion.get(indice_padre2).get(i);
+                hijo.add(new Integer(temp.intValue()));
+            }
+            for (int i = punto2; i < tam_genes; i++) {
+                Integer temp =poblacion.get(indice_padre1).get(i);
+                hijo.add(new Integer(temp.intValue()));
+            }
+        }else{
+            
+            for (int i = 0; i < punto2; i++){ 
+                Integer temp =poblacion.get(indice_padre1).get(i);
+                hijo.add(new Integer(temp.intValue()));
+            }
+            for (int i = punto2; i < punto1; i++) {
+                Integer temp =poblacion.get(indice_padre2).get(i);
+                hijo.add(new Integer(temp.intValue()));
+            }
+            for (int i = punto1; i < tam_genes; i++) {
+                Integer temp =poblacion.get(indice_padre1).get(i);
+                hijo.add(new Integer(temp.intValue()));
+            }
+        }
+        
+    }
+
+    private static void mutacion(List<Integer> hijo) {
+        Random rnd = new Random();
+        int gen;
+        gen = rnd.nextInt(tam_genes);
+        if (hijo.get(gen) == 0) {
+            hijo.set(gen, 1);
+        }else{
+            hijo.set(gen, 0);
+        }
+    }
+
+    private static int obtener_mejor() {
+        int indice_mejor = 0;
+        int score_mejor = obtenerScore(poblacion.get(0));
+        for (int i = 1; i < tam_poblacion; i++) {
+            int score = obtenerScore(poblacion.get(i));
+            if (score > score_mejor) {
+                indice_mejor = i;
+                score_mejor = score;
+            }
+        }
+        return indice_mejor;
     }
     
 }
